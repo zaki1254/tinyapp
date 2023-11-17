aad79266825628d76f1d23551db20ae78d8cc36a
zaki1254 <zakessa10@gmail.com>
2023-11-17 06:34:23 -0500

    Added header

--- express_server.js
+++ express_server.js
@@ -8,6 +8,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
